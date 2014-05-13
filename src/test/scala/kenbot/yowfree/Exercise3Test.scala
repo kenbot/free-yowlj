4d6c51cd0ee64adfa2c6cfe8604dc4a3bb28c676
Andrew Herron <thespyder@programmer.net>
2014-05-13 22:59:43 +1000

    impure delete is now actually testing impure :)

--- src/test/scala/kenbot/yowfree/Exercise3Test.scala
+++ src/test/scala/kenbot/yowfree/Exercise3Test.scala
@@ -15,25 +15,25 @@ class Exercise3Test extends FunSpec with ShouldMatchers {
       val end = interpretPure(put(Key("a"), Value("x")), start)
       end should equal (Map(Key("a") -> Value("x")))
     }
-    
+
     it("should interpret Delete correctly") {
       val start = Map(Key("DELETEME") -> Value("XXX"))
       val end = interpretPure(delete(Key("DELETEME")), start)
       end should equal (Map())
     }
-    
-    
+
+
     it("should follow a script faithfully") {
       val script = for {
         a <- get(Key("a"))
         _ <- put(Key("b"), a)
         _ <- delete(Key("c"))
       } yield ()
-      
-      val start = Map(Key("a") -> Value("apples"), 
+
+      val start = Map(Key("a") -> Value("apples"),
                       Key("c") -> Value("DELETE ME"))
       val end = interpretPure(script, start)
-      
+
       end should equal (Map(Key("a") -> Value("apples"),
                             Key("b") -> Value("apples")))
     }
@@ -45,14 +45,14 @@ class Exercise3Test extends FunSpec with ShouldMatchers {
       interpretImpure(put(Key("a"), Value("x")), mutableStore)
       mutableStore should equal (Map(Key("a") -> Value("x")))
     }
-    
+
     it("should interpret Delete correctly") {
-      val start = Map(Key("DELETEME") -> Value("XXX"))
-      val end = interpretPure(delete(Key("DELETEME")), start)
-      end should equal (Map())
+      val mutableStore = mutable.Map[Key, Value](Key("DELETEME") -> Value("XXX"))
+      val end = interpretImpure(delete(Key("DELETEME")), mutableStore)
+      mutableStore should equal (Map())
     }
-    
-    
+
+
     it("should follow a script faithfully") {
 
       val script = for {
@@ -60,12 +60,12 @@ class Exercise3Test extends FunSpec with ShouldMatchers {
         _ <- put(Key("b"), a)
         _ <- delete(Key("c"))
       } yield ()
-      
-      val mutableStore = mutable.Map(Key("a") -> Value("apples"), 
+
+      val mutableStore = mutable.Map(Key("a") -> Value("apples"),
                                      Key("c") -> Value("DELETE ME"))
-                                     
+
       interpretImpure(script, mutableStore)
-      
+
       mutableStore should equal (Map(Key("a") -> Value("apples"),
                                      Key("b") -> Value("apples")))
     }
